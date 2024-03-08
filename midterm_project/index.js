6c97c79498e51225e00403f6c892bcf3759aaa13
Annie Huynh <annieh47@iastate.edu>
2024-03-07 19:29:16 -0600

    added a few things

--- midterm_project/index.js
+++ midterm_project/index.js
@@ -1,23 +1,27 @@
+//Diana Nguyen
+//dianan64@iastate.edu
+//Feb 24, 2024
+
 fetch("./data.json")
   .then((response) => response.json())
-  .then((datesAvailable) => loadMovies(datesAvailable));
+  .then((myData) => loadData(myData));
 
-function loadDates(myMovies){
+function loadData(myData){
     var container = document.getElementById("goodmovies");
-    // console.log(myMovies);
+    console.log(myMovies);
     for (let i = 0; i < myMovies.movies.length; i++){
-        let date = myMovies.movies[i].date;
+        let title = myMovies.movies[i].title;
         let year = myMovies.movies[i].year;
-        // let url = myMovies.movies[i].url;
+        let url = myMovies.movies[i].url;
         
-        // console.log(title);
+        console.log(title);
         //construct the HTML element
         let division = document.createElement("div");
 
         division.innerHTML = `
-        <h3>${date}
+        <h3>${title}
         ${year}
-        // <img src=${url} />
+        <img src=${url} />
         `;
         container.appendChild(division);
     }//end of for
