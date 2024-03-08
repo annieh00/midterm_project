cd9a0d800e01a9ddfd3138239d7b99e3faf74494
Annie Huynh <annieh47@iastate.edu>
2024-03-08 00:05:56 -0600

    added pictures

--- midterm_project/index.js
+++ midterm_project/index.js
@@ -1,29 +1,69 @@
-//Diana Nguyen
-//dianan64@iastate.edu
-//Feb 24, 2024
+// script.js
 
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((myData) => loadData(myData));
+// Function to fetch JSON data from the file
+function fetchData() {
+  return fetch('data.json')
+      .then(response => response.json());
+}
 
-function loadData(myData){
-    var container = document.getElementById("goodmovies");
-    console.log(myMovies);
-    for (let i = 0; i < myMovies.movies.length; i++){
-        let title = myMovies.movies[i].title;
-        let year = myMovies.movies[i].year;
-        let url = myMovies.movies[i].url;
-        
-        console.log(title);
-        //construct the HTML element
-        let division = document.createElement("div");
+// Function to display the JSON data as a list
+function displayDates() {
+  var dateList = document.getElementById("dateList");
 
-        division.innerHTML = `
-        <h3>${title}
-        ${year}
-        <img src=${url} />
-        `;
-        container.appendChild(division);
-    }//end of for
+  fetchData()
+      .then(data => {
+          data.availableDates.forEach(function (item) {
+              var listItem = document.createElement("li");
+              listItem.textContent = item.date + ", " + item.year;
+              dateList.appendChild(listItem);
+          });
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error);
+      });
+}
 
-}//end of function
+// Call the displayDates function when the page is loaded
+window.onload = displayDates;
+
+// annie was here 
+// Function to display the JSON data as a list
+function displayNail() {
+  var dateList = document.getElementById("nailList");
+
+  fetchData()
+      .then(data => {
+          data.nail_prices.forEach(function (item) {
+              var listItem = document.createElement("li");
+              listItem.textContent = item.type1 + ": " + item.price1;
+              dateList.appendChild(listItem);
+          });
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error);
+      });
+}
+
+// Call the displayDates function when the page is loaded
+window.onload = displayNail;
+
+// Function to display the JSON data as a list
+function displayHair() {
+  var dateList = document.getElementById("hairList");
+
+  fetchData()
+      .then(data => {
+          data.hair_prices.forEach(function (item) {
+              var listItem = document.createElement("li");
+              listItem.textContent = item.type2 + ": " + item.price2;
+              dateList.appendChild(listItem);
+          });
+      })
+      .catch(error => {
+          console.error('Error fetching data:', error);
+      });
+}
+
+// Call the displayDates function when the page is loaded
+window.onload = displayHair;
+
